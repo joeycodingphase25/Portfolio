818a2b60a5a5e2cd99ab8a2d9ac22f086b444887
Joseph Madenjian <joseph.madenjian@gmail.com>
2022-06-25 19:20:26 -0400

    created player and canvas JS

---
+++ marioJS/index.js
@@ -0,0 +1,38 @@
+
+
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+canvas.width = window.innerWidth
+canvas.height = innerHeight
+
+
+console.log(c)
+
+// ES6 Class
+class Player {
+    constructor() {
+        this.position = {
+            x: 100,
+            y: 100
+        };
+        this.width = 30;
+        this.height = 30;
+    }
+
+    draw() {
+        c.fillStyle = "red";
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+
+}
+
+const player = new Player();
+player.draw()
+
+
+
+
+
+
+

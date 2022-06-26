70dcce56cc0b80997730269af58414c65bef8fa7
Joseph Madenjian <joseph.madenjian@gmail.com>
2022-06-25 21:51:53 -0400

    background scroller

--- marioJS/index.js
+++ marioJS/index.js
@@ -43,7 +43,34 @@ class Player {
     // end player class
 }
 
+class Platform {
+    constructor({x, y}) {
+        this.position = {
+            x: x,
+            y: y
+        }
+
+        this.width = 200;
+        this.height = 20;
+
+        // end constructor
+    }
+    draw() {
+        c.fillStyle = 'blue'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+    // end platform class
+}
+
+
+
+
 const player = new Player();
+const platforms = [new Platform({
+    x:200,y: 100
+}),new Platform({
+    x: 500, y:200
+})]
 const keys = {
     right: {
         pressed: false
@@ -57,12 +84,35 @@ function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0, 0, canvas.width, canvas.height)
     player.update()
+    platforms.forEach(platform => {
+        platform.draw()
+    })
 
-    if (keys.right.pressed) {
+    if (keys.right.pressed && player.position.x < 400) {
         player.velocity.x = 5
-    } else if (keys.left.pressed) {
+    } else if (keys.left.pressed && player.position.x >100) {
         player.velocity.x = -5
-    } else { player.velocity.x = 0}
+    } else { 
+        player.velocity.x = 0
+        
+        if (keys.right.pressed) {
+            platforms.forEach(platform => {
+                platform.position.x -= 5
+            })
+        } else if (keys.left.pressed) {
+            platforms.forEach(platform => {
+                platform.position.x += 5
+            })
+        }
+    }
+
+    // platform collision
+    platforms.forEach(platform => {
+        if (player.position.y + player.height <= platform.position.y && player.position.y + player.height + player.velocity.y >= platform.position.y && player.position.x + player.width >= platform.position.x && player.position.x <= platform.position.x + platform.width) {
+            player.velocity.y = 0
+        }
+    // end animate function
+    })
 }
 
 animate()
